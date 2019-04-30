256860669c61e1eec5bdb32c1572d4688a770521
Samuel85 <50142279+Samuel85@users.noreply.github.com>
2019-04-30 14:51:26 +0200

    Add files via upload
    Class for a CTRW under an harmonic oscillator (Ornstein-Uhlenbeck process) simulation.
    See test for an example.

---
+++ CTRW_Ornstein_Uhlenbeck.m
@@ -0,0 +1,63 @@
+classdef CTRW_Ornstein_Uhlenbeck < handle
+properties
+    ds
+    dt
+    numberOfSeconds
+    gamma
+    xs
+    ts
+    x
+    alpha
+    t
+end
+methods
+    function obj = CTRW_Ornstein_Uhlenbeck()
+        obj.ds              = 0.0001;
+        obj.dt              = 0.001;
+        obj.numberOfSeconds = 1;
+        obj.gamma = 1;
+    
+        obj.xs      = 1;
+        obj.ts      = 0;
+        obj.alpha   = 1;
+    end
+    function start(obj)
+        obj.t = 0:obj.dt:obj.numberOfSeconds;
+        obj.x=zeros(length(obj.t),1);
+        
+        for j=1:length(obj.t)     
+            while obj.ts < obj.t(j)
+                % Generate the jumps n(s,ds)
+                % Gaussian pdf with variance sigma^2 = ds
+                % note: normrnd needs the std as a second parameter
+                ns = normrnd(0,sqrt(obj.ds),1,1);
+
+                % Ec. 12a
+                obj.xs = obj.xs - obj.ds*obj.gamma*obj.xs + ns;
+
+                % calculate waiting time, tau(s,ds);
+                u1 = rand(1,1);
+                u2 = rand(1,1);
+
+                % Ec. A1
+                v = pi*(u1-1/2);        
+                % Ec. A2
+                w = -log(u2);
+
+                tau = (obj.ds^(1/obj.alpha))*(sin(obj.alpha*(v+pi/2))/(cos(v)^(1/obj.alpha)))*...
+                      (cos(v-obj.alpha*(v+pi/2))/w)^((1-obj.alpha)/obj.alpha);
+
+                % Ec. 12b
+                obj.ts = obj.ts + tau;        
+            end
+        obj.x(j)=obj.xs;        
+        end        
+    end 
+    function plot(obj)
+        figure;
+        plot(obj.t,obj.x);
+        xlabel('time[s]');
+        title(['\alpha=',num2str(obj.alpha)]);
+    end     
+end
+end
---
+++ test.m
@@ -0,0 +1,14 @@
+clc;clear all;
+
+myCTRW = CTRW_Ornstein_Uhlenbeck;
+myCTRW.numberOfSeconds = 100;
+myCTRW.gamma = 0;
+myCTRW.alpha = 0.5;
+myCTRW.start;
+
+%%
+myCTRW.plot;
+
+x = myCTRW.x;
+x = x - mean(x);
+entropyAutoregressiveModel(x)
